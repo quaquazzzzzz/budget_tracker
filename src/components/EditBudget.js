2c46eb3e171055ff8662a656ed965219f79272e4
quaquazzzzz <annabawor.ab@googlemail.com>
2024-03-15 16:52:10 +0100

    verbesserungen

--- src/components/EditBudget.js
+++ src/components/EditBudget.js
@@ -7,14 +7,14 @@ const EditBudget = (props) => {
 			<input
 				required='required'
 				type='number'
-				class='form-control mr-3'
+				className='form-control mr-3'
 				id='name'
 				value={value}
 				onChange={(event) => setValue(event.target.value)}
 			/>
 			<button
 				type='button'
-				class='btn btn-primary'
+				className='btn btn-primary'
 				onClick={() => props.handleSaveClick(value)}
 			>
 				Save
