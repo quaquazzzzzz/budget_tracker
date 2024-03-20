df4a4d3dd06bcea1e6538d133a22cf59718d9c8f
quaquazzzzz <annabawor.ab@googlemail.com>
2024-03-20 11:18:42 +0100

    changes20505050

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,6 @@
-describe('Budget App', () => {
-  it('sollte die Budget-Eingabe speichern', () => {
+describe('Budget Tracker Einfach', () => {
+  it('sollte den Haupttitel anzeigen', () => {
     cy.visit('https://quaquazzzzzz.github.io/budget_tracker/'); // URL deiner Anwendung
-    cy.get('#budget-input').type('2000'); // Eingabefeld für Budget, ID anpassen
-    cy.contains('button', 'Save').click(); // Speichern-Button klicken
-    // Der nachfolgende Selector muss an das Element angepasst werden, das den gespeicherten Wert anzeigt
-    cy.get('#saved-budget').should('contain', '2000'); // Überprüfe, ob "2000" im Element mit der ID "saved-budget" erscheint
+    cy.contains('Mein Budget Tracker').should('be.visible'); // Überprüft, ob der Titel sichtbar ist
   });
 });
